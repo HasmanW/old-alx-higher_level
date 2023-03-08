54ff76466917ec3b676a78e9bb4aae6c8431e469
The_Tutor <hasmanwasilwaw@gmail.com>
2023-03-08 21:49:54 +0300

    tasks

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,6 +2,4 @@
 for i in reversed(range(65, 91)):
     if i % 2 == 0:
         i += 32
-        print(chr(i), end="")
-    else:
-        print(chr(i), end="")
+    print("{}".format(chr(i)), end="")
