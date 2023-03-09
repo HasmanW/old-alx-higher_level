b75b78435d78ea65ab030851f6e7b16128f401a8
The_Tutor <hasmanwasilwaw@gmail.com>
2023-03-09 05:59:25 +0300

    tasks

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new_str = ""
+    for i in range(len(str)):
+        if ord(str[i]) in range(65, 91):
+            new_str += str[i]
+        else:
+            new_str += chr(ord(str[i]) - 32)
+    return new_str
