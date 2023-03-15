ead16b40a2a367e92164b7eb083c9fd632327768
The_Tutor <hasmanwasilwaw@gmail.com>
2023-03-15 21:39:31 +0300

    task update

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        return
+    else:
+        str_len = len(sentence)
+        first_char = sentence[0]
+        return str_len, first_char
