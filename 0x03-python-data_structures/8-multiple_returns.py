8b00916852f39fa1fa03259096d5f050a5c8e6f4
The_Tutor <hasmanwasilwaw@gmail.com>
2023-03-15 21:54:39 +0300

    task update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return
+        return 0, None
     else:
         str_len = len(sentence)
         first_char = sentence[0]
-        return (str_len, first_char)
+        return str_len, first_char
