44cec98ccb3d8f30d8d5df885979b4eb7436f5f1
The_Tutor <hasmanwasilwaw@gmail.com>
2023-03-17 22:46:33 +0300

    tasks

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    new_set = []
     for val in set_1:
         if val in set_2:
-            return [val]
+            new_set.append(val)
+    return new_set
