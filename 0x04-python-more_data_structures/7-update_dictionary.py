d2ea29fa79af8c365ae1774536d3fd451fae4d4e
The_Tutor <hasmanwasilwaw@gmail.com>
2023-03-18 00:07:29 +0300

    tasks

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -4,7 +4,6 @@ def update_dictionary(a_dictionary, key, value):
     keys = []
     for k, v in a_dictionary.items():
         keys.append(k)
-    
     if key in keys:
         a_dictionary[key] = value
     else:
