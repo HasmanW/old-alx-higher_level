6a1abb71489e94bf9163e87ac532c779d1b2980d
The_Tutor <hasmanwasilwaw@gmail.com>
2023-03-17 22:14:09 +0300

    tasks

---
+++ 0x04-python-more_data_structures/ 2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    copy_list = []
+    result = 0
+
+    for i in my_list:
+        if i not in copy_list:
+            copy_list.append(i)
+    for x in copy_list:
+        result += x
+    return result
