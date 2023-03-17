e101b8985c1c448b2a38037f0f52409fa63fc3a7
The_Tutor <hasmanwasilwaw@gmail.com>
2023-03-17 22:43:51 +0300

    tasks

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -8,5 +8,4 @@ def only_diff_elements(set_1, set_2):
     for x in set_2:
         if x not in set_1:
             new_set.append(x)
-
     return new_set
--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -4,5 +4,4 @@ def number_keys(a_dictionary):
     keys = []
     for key, value in a_dictionary.items():
         keys.append(key)
-        num_keys = len(keys)
-    return num_keys
+    return len(keys)
