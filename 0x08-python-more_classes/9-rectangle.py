1f00092f3f3ff9c3a035de1e5dcf13a37309ff5d
The_Tutor <hasmanwasilwaw@gmail.com>
2023-04-04 04:44:37 +0300

    task update

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -71,10 +71,7 @@ class Rectangle:
         new_str = ""
         for height in range(self.__height):
             for width in range(self.__width):
-                try:
-                    new_str += str(self.print_symbol)
-                except Exception:
-                    new_str += type(self).print_symbol
+                new_str += str(self.print_symbol)
             new_str += "\n"
         return new_str
 
