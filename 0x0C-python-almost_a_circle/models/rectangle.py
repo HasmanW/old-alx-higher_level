ab1fc6eadcb62cf0c56d2410429263daedeb6ad3
The_Tutor <hasmanwasilwaw@gmail.com>
2023-04-30 04:26:00 +0300

    tasks

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,31 +6,16 @@ from models.base import Base
 class Rectangle(Base):
     """ The rectangle class """
     def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
-        super().__init__(id)
 
     @property
     def width(self):
         """ returns the width """
         return self.__width
-
-    @property
-    def height(self):
-        """ returns the height"""
-        return self.__height
-
-    @property
-    def x(self):
-        """ returns value x """
-        return self.__x
-
-    @property
-    def y(self):
-        """ returns value y """
-        return self.__y
 
     @width.setter
     def width(self, value):
@@ -41,14 +26,24 @@ class Rectangle(Base):
             raise ValueError("width must be > 0")
         self.__width = value
 
+    @property
+    def height(self):
+        """ returns the height"""
+        return self.__height
+
     @height.setter
     def height(self, value):
         """ height setting and validation """
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
+
+    @property
+    def x(self):
+        """ returns value x """
+        return self.__x
 
     @x.setter
     def x(self, value):
@@ -58,6 +53,11 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
+
+    @property
+    def y(self):
+        """ returns value y """
+        return self.__y
 
     @y.setter
     def y(self, value):
